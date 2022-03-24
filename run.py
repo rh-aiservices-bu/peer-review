00a64017938d3546e8a515d01384a526a7297156
Prasanth Anbalagan <panbalag@redhat.com>
2022-03-24 22:12:43 +0000

    Scaled down model and artifacts

--- run.py
+++ run.py
@@ -4,43 +4,38 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from datetime import datetime
 import inquirer
+import glob
 
 notebook_path = 'notebooks/'
 reports_path = 'reports/'
-notebooks = ['01-explore-data.ipynb', '02-build-features.ipynb', '03-train-model.ipynb', '04-tune-model.ipynb', '05-model-inference.ipynb']
+notebooks = []
 date = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
 os.mkdir(reports_path+'run_' + date)
 
-#for file in notebooks:
-#    with open(notebook_path + file) as f:
-#        nb = nbformat.read(f, as_version=4)
-#    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-#    ep.preprocess(nb, {'metadata': {'path': notebook_path}})
-#    with open(reports_path +'run_' + date + '/' + file, 'w', encoding='utf-8') as f:
-#        nbformat.write(nb, f)
+for file in glob.glob(notebook_path + '*.ipynb'):
+    notebooks.append(file)
+notebooks.sort()
 
 def execute_notebook(notebook, timeout=600):
-    with open(notebook_path + notebook) as f:
+    with open(notebook) as f:
         nb = nbformat.read(f, as_version=4) 
     ep = ExecutePreprocessor(timeout=timeout, kernel_name='python3')
     ep.preprocess(nb, {'metadata': {'path': notebook_path}})
         
-    with open(reports_path +'run_' + date + '/' + notebook, 'w', encoding='utf-8') as f:
+    with open(reports_path +'run_' + date + '/' + notebook, 'w+', encoding='utf-8') as f:
         nbformat.write(nb, f)
     print("Execute notebook - " + notebook + ":Complete!")
     print("Please check the rerun results here: " + reports_path +'run_' + date + '/' + notebook)
-
 
 def select_operation():
     questions = [
         inquirer.List('notebook',
                     message="Which notebook would you like to rerun?",
-                    choices=['01-explore-data.ipynb', '02-build-features.ipynb', '03-train-model.ipynb', '04-tune-model.ipynb', '05-model-inference.ipynb'],
+                    choices=notebooks,
                 ),
     ]
     answers = inquirer.prompt(questions)
     return answers
-
 
 result = select_operation()
 execute_notebook(result["notebook"], 600)

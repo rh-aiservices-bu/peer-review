03aeaa262d8c8c012a42b03fbd7cbdc6dc647a20
Prasanth Anbalagan <panbalag@redhat.com>
2022-03-26 04:29:05 +0000

    Update imputer to use median strategy

--- prediction.py
+++ prediction.py
@@ -2,10 +2,18 @@ import pandas as pd
 import logging
 import joblib
 import json
+import os
+import shutil
+import configparser
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
+import numpy as np
 biomarkers = None
 pipeline = None
 model = None
+settings = None
 
 def transformAndPredict(raw, pipeline, model, dropNonBioMarkers = True) :
 
@@ -17,6 +25,7 @@ def transformAndPredict(raw, pipeline, model, dropNonBioMarkers = True) :
         dropped = raw
 
     # perform inference and predict
+
     transformed = pipeline.transform(dropped)
     prediction = model.predict(transformed)
     return prediction
@@ -29,8 +38,23 @@ def predict(data):
     #Load the data to run prediction on, model and pipeline
     jsondata = json.loads("[" + json.dumps(data) + "]")
     raw = pd.DataFrame(jsondata)
-    pipeline = joblib.load("data/transform/pipeline.pkl")
-    model = joblib.load("models/experiment/xgbc_model.pkl")
+    settings = configparser.ConfigParser()
+    settings._interpolation = configparser.ExtendedInterpolation()
+    settings.read('config')
+    settings.sections()
+
+    #path to data folder
+    version = settings.get('Run', 'version')
+
+    #Prepare data directory for current run
+    data_path = settings.get('Path', 'data') + "run_" + version
+    transform_path = data_path + "/transform/"
+    model_path = settings.get('Path', 'models') + "run_" + version
+
+    
+    #Load the data to run prediction on, model and pipeline
+    pipeline = joblib.load(transform_path + "/pipeline.pkl")
+    model = joblib.load(model_path + "/final/rfc_model.pkl")
     
     # Run prediction
     prediction = transformAndPredict(raw, pipeline, model, dropNonBioMarkers = False )
@@ -38,4 +62,3 @@ def predict(data):
 
     return {'prediction': result}
 
-    
